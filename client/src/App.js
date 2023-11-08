8c5d540efb866e1f892f6314aff0172507339980
X <63634734+GreenTeaDay@users.noreply.github.com>
2023-11-08 14:49:48 -0600

    Pass JWT token from local storage to QuoteForm for authenticated requests

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import QuoteForm from './components/QuoteForm';
+const token = localStorage.getItem('token');
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/login" element={<Login />} />
-          <Route path="/quote" element={<QuoteForm />} />
+          <Route path="/quote" element={<QuoteForm token={token} />} />
           {/* Add more routes as needed */}
         </Routes>
       </div>
--- client/src/components/QuoteForm.js
+++ client/src/components/QuoteForm.js
@@ -8,73 +8,91 @@ function QuoteForm({ token }) {
   const [estimatedCost, setEstimatedCost] = useState('');
   const [status, setStatus] = useState('Pending');
 
+  // New state to store the quote response
+  const [quoteResponse, setQuoteResponse] = useState(null);
+  const [error, setError] = useState('');
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    setError('');
     try {
       const response = await axios.post(`${process.env.REACT_APP_API_BASE_URL}/quotes`, 
         { weight, originPostalCode, destPostalCode, estimatedCost, status },
         { headers: { Authorization: `Bearer ${token}` } }
       );
-      
-      // Handle the response from generating a quote
-      console.log('Quote generated:', response.data);
-      // You may want to redirect to another page or display the quote details
-    } catch (error) {
-      console.error('Error generating quote:', error);
-      // Handle error
+      setQuoteResponse(response.data); // Store the quote response
+    } catch (err) {
+      setError('Error generating quote: ' + err.message);
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h2>Generate a Quote</h2>
-      <div>
-        <label>Weight:</label>
-        <input
-          type="number"
-          value={weight}
-          onChange={e => setWeight(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <label>Origin Postal Code:</label>
-        <input
-          type="number"
-          value={originPostalCode}
-          onChange={e => setOriginPostalCode(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <label>Destination Postal Code:</label>
-        <input
-          type="number"
-          value={destPostalCode}
-          onChange={e => setDestPostalCode(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <label>Estimated Cost:</label>
-        <input
-          type="number"
-          value={estimatedCost}
-          onChange={e => setEstimatedCost(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <label>Status:</label>
-        <select value={status} onChange={e => setStatus(e.target.value)}>
-          <option value="Pending">Pending</option>
-          <option value="Confirmed">Confirmed</option>
-          <option value="Cancelled">Cancelled</option>
-        </select>
-      </div>
-      <button type="submit">Submit Quote</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h2>Generate a Quote</h2>
+        <div>
+          <label>Weight:</label>
+          <input
+            type="number"
+            value={weight}
+            onChange={e => setWeight(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Origin Postal Code:</label>
+          <input
+            type="number"
+            value={originPostalCode}
+            onChange={e => setOriginPostalCode(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Destination Postal Code:</label>
+          <input
+            type="number"
+            value={destPostalCode}
+            onChange={e => setDestPostalCode(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Estimated Cost:</label>
+          <input
+            type="number"
+            value={estimatedCost}
+            onChange={e => setEstimatedCost(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Status:</label>
+          <select value={status} onChange={e => setStatus(e.target.value)}>
+            <option value="Pending">Pending</option>
+            <option value="Confirmed">Confirmed</option>
+            <option value="Cancelled">Cancelled</option>
+          </select>
+        </div>
+        <button type="submit">Submit Quote</button>
+      </form>
+      
+      {quoteResponse && (
+        <div>
+          <h2>Quote Generated Successfully</h2>
+          <p>Weight: {quoteResponse.weight}</p>
+          <p>Origin Postal Code: {quoteResponse.originPostalCode}</p>
+          <p>Destination Postal Code: {quoteResponse.destPostalCode}</p>
+          <p>Estimated Cost: {quoteResponse.estimatedCost}</p>
+          <p>Status: {quoteResponse.status}</p>
+        </div>
+      )}
+      {error && (
+        <div>
+          <p className="error">{error}</p>
+        </div>
+      )}
+    </div>
   );
 }
 
